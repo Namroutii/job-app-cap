bcf5c7eef7c340e1f2311f92d88e7bd65576acd6
dalia khalili <khalilidalia58@gmail.com>
2022-11-16 00:18:41 +0200

    header

--- job-app/src/App.js
+++ job-app/src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import Footer from "./components/footer/Footer";
 import Card from "./components/services/Services";
 import Home from "./pages/home/Home";
+import Header from "./components/header/Header"
 function App() {
   return (
     <div className="App">
       <Home />
+      <Header/>
       <Card/>
       <Footer/>
+      
     </div>
 
   );
