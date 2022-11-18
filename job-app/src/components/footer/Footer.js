9e9a7cba0fc8b5c5322d9ff58f2f2a2b4ea06c87
AbdullahSO <abd.s.othman@gmail.com>
2022-11-18 13:45:46 +0200

    change the footer

--- job-app/src/components/footer/Footer.js
+++ job-app/src/components/footer/Footer.js
@@ -1,20 +1,33 @@
 import "./Footer.css";
 const Footer = () => {
     return (
-        <footer id="footer">
-                <div className="logo-motto">
-                    <img className="footer-logo" src="./images/Logo-Footer.png" alt=""></img>
-                    <h5><span className="footer-motto"></span></h5>
+    <footer class="p-1 text-center">
+        <section id="contact-info" class="p-5">
+            <div class="container">
+                <div class="row g-4">
+                    <h2 class="text-center mb-3 contact-info-title">Contact Information </h2>
+                    <div className="logo-motto col">
+                        <img className="footer-logo " src="./images/Logo-Footer.png" alt=""></img>
+                        <h5><span className="footer-motto">Trust is all you need </span></h5>
+                    </div>
+                    < div className="footer-contact col">
+                        <span class="p-1 fw-bold loc">Location: Nablus Rafidia st</span>
+                        <span class="p-1 fw-bold phone">Phone: 092355555</span>  
+                        <span class="p-1 fw-bold email">Email: hireme@gmail.com </span>
+                    </div> 
+                    <div class="col">
+                        <div><h4 class="social-title">Social media:</h4></div>
+                        <a href="#" class="fa fa-facebook"></a>
+                        <a href="#" class="fa fa-twitter"></a>
+                        <a href="#" class="fa fa-linkedin"></a>
+                    </div>
                 </div>
-                < div className="footer-contact">
-                    <div className="p-1"><span class="fw-bold loc">Location: Nablus 24st</span></div>
-                    <div className="p-1"><span class="fw-bold phone">Phone: 092355555</span></div>    
-                    <div className="p-1"><span class="fw-bold email">Email: hireme@gmail.com </span></div>
-                </div> 
-                < div className="socials">
-                    
-                </div> 
-        </footer>
+            </div>
+        </section>
+        <div class="container">
+            <p class="lead copy-right">&copy; C.A.P. NNU </p>
+        </div>
+    </footer>
     )
 }
 export default Footer
