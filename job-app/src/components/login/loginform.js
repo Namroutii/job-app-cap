d538662c2cee6733547146b83ed484e958a8aff8
NAMROUTI\GoldenTech <s12116442@stu.najah.edu>
2022-11-18 20:59:34 +0200

    login page

--- job-app/src/components/login/loginform.js
+++ job-app/src/components/login/loginform.js
@@ -1,25 +1,20 @@
 
+import React from "react"
 import "../login/loginform.css"
 
 const LoginForm = () => {
+
     return (
-
         <div className="cover">
-            <h1>Login to HireMe</h1>
-
-            <input type="text" placeholder="Enter your username" />
-            <input type="password" placeholder="Enter your password" />
-
-            <div className="login-btn">Login</div>
-
+            <h1>HireMe Login</h1>
+            <input type="text" placeholder="username" />
+            <input type="password" placeholder="password" />
+            <div className="login-btn" >Login</div>
             <p className="text">Or login using</p>
-
             <div className="alt-login">
                 <div className="facebook"></div>
-                <div className="google">
-                </div>
+                <div className="google"></div>
             </div>
-
         </div>
     )
 }
