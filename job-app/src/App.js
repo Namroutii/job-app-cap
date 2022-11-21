af01af80788e284cb298baa53a7f030268785961
osamashouli <101651050+osamashouli@users.noreply.github.com>
2022-11-21 23:10:26 +0200

    making table of cvs

--- job-app/src/App.js
+++ job-app/src/App.js
@@ -4,16 +4,11 @@ import Header from "./components/header/Header";
 import LoginForm from "./components/login/loginform";
 import NavBar from "./components/NavBar/NavBar";
 import Services from "./components/services/Services";
+import GetCV from "./pages/getCV/GetCV";
 function App() {
   return (
     <div>
-      <NavBar/>
-      <Header/>
-      <Services/>
-      <Footer/>
-      <div className="login-page">
-        <LoginForm />
-      </div>
+      <GetCV />
     </div>
   );
 }
--- job-app/src/pages/home/Home.js
+++ job-app/src/pages/home/Home.js
@@ -1,12 +1,5 @@
-
-
 import "./Home.css";
 function Home() {
-  return (
-    <div>
-      
-      
-    </div>
-  );
+  return <div></div>;
 }
 export default Home;
