eb023cdabbb50108eee48299b56a684e081f6e81
NAMROUTI\GoldenTech <s12116442@stu.najah.edu>
2022-11-25 18:01:34 +0200

    Add CV form

--- job-app/src/components/forms/Form.js
+++ job-app/src/components/forms/Form.js
@@ -2,15 +2,15 @@ import './Form.css';
 
 
 const Form = (props) => {
-return (
-   <>
-      <form>
-      <div className='form'>
-            <label > {props.children} </label>
-            <input type="text" placeholder={props.children} required />
-      </div>
-      </form>
-   </>
+   return (
+      <>
+         <form>
+            <div className='form'>
+               <label > {props.children} </label>
+               <input type="text" placeholder={props.children} required />
+            </div>
+         </form>
+      </>
    )
 
 }
--- job-app/src/pages/addCV/AddCV.js
+++ job-app/src/pages/addCV/AddCV.js
@@ -4,9 +4,9 @@ import Form from "../../components/forms/Form";
 import "./AddCV.css";
 import { useState } from "react";
 
+
 function AddCV() {
   const [showSelect, setShowSelect] = useState("");
-
   const handleShowSelect = (event) => {
     const getuser = event.target.value;
     setShowSelect(getuser);
@@ -15,43 +15,71 @@ function AddCV() {
   return (
     <div>
       <NavBar /> <br />
-      <div className=" title">Basic Information</div>
+      <div className="title">Add your CV Down here </div>
       <hr />
       <Form>Full Name</Form>
+      <Form>Date of birth</Form>
       <Form>Address</Form>
       <Form>Phone Number</Form>
-      <div className="email-input">
-        <label className="emailLabel"> Email</label> <br />
-        <input type="email" placeholder="Email" className="email-input" />
-      </div>
-      <Form>Years of Experience</Form>
-      <Form>Language</Form>
-      <Form>Skills</Form>
-      <Form>Date of birth</Form>
-      <Form>Career</Form>
+      <Form>Email</Form>
       <Form>Degree</Form>
-      <Form>Soft skills</Form>
-      <Form>LinkedIn URL</Form>
       <div className="select">
         <label> Department:</label> <br />
         <select onChange={(e) => handleShowSelect(e)}>
           <option selected>Select your job:</option>
-          <option value="1">Accounting </option>
-          <option value="2">Programmer </option>
+          <option value="1">Programmer </option>
+          <option value="2">Accountant </option>
           <option value="3">Designer </option>
-          <option value="3">Cyber security</option>
+          <option value="4">Cyber security</option>
         </select>
       </div>
-      {showSelect === "1" && (    //here but your box for the job 
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="exampleInputEmail1"
-          />
+      {showSelect === "1" && (
+        <div>
+          <Form>Career</Form>
+          <Form>Years of Experience</Form>
+          <Form>Soft skills</Form>
+          <Form>LinkedIn URL</Form>
+          <Form>Language</Form>
+          <Form>Skills</Form>
+          <br /><br />
+          <button type="submit">submit</button>
+          <br /><br />
+        </div>
+      )}
+      {showSelect === "2" && (
+        <div>
+          <Form>Career</Form>
+          <Form>Years of Experience</Form>
+          <Form>Soft skills</Form>
+          <Form>LinkedIn URL</Form>
+          <Form>Language</Form>
+          <Form>Skills</Form>
+          <br /><br />
+          <button type="submit">submit</button>
+        </div>
+      )}
+      {showSelect === "3" && (
+        <div>
+          <Form>Career</Form>
+          <Form>Years of Experience</Form>
+          <Form>Soft skills</Form>
+          <Form>LinkedIn URL</Form>
+          <Form>Language</Form>
+          <Form>Skills</Form>
+          <br /><br />
+          <button type="submit">submit</button>
+        </div>
+      )}
+      {showSelect === "4" && (
+        <div>
+          <Form>Career</Form>
+          <Form>Years of Experience</Form>
+          <Form>Soft skills</Form>
+          <Form>LinkedIn URL</Form>
+          <Form>Language</Form>
+          <Form>Skills</Form>
+          <br /><br />
+          <button type="submit">submit</button>
         </div>
       )}
       <Footer />
