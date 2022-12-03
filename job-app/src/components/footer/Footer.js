b8785392a68d70f51c6c350eb2c5a15d2579c258
AbdullahSO <abd.s.othman@gmail.com>
2022-12-03 18:54:58 +0200

    edit footer

--- job-app/src/components/footer/Footer.js
+++ job-app/src/components/footer/Footer.js
@@ -1,31 +1,31 @@
 import "./Footer.css";
 const Footer = () => {
     return (
-    <footer class="p-1 text-center">
-        <section id="contact-info" class="p-5">
-            <div class="container">
-                <div class="row g-4">
-                    <h2 class="text-center mb-3 contact-info-title">Contact Information </h2>
+    <footer className="text-center">
+        <section id="contact-info" className="p-1">
+            <div className="container">
+                <div className="row g-4">
+                    <h2 className="text-center mb-3 contact-info-title">Contact Information </h2>
                     <div className="logo-motto col">
                         <img className="footer-logo " src="./images/Logo-Footer.png" alt=""></img>
                         <h5><span className="footer-motto">Trust is all you need </span></h5>
                     </div>
                     < div className="footer-contact col">
-                        <span class="p-1 fw-bold loc">Location: Nablus Rafidia st</span>
-                        <span class="p-1 fw-bold phone">Phone: 092355555</span>  
-                        <span class="p-1 fw-bold email">Email: hireme@gmail.com </span>
+                        <span className="p-1 fw-bold loc">Location: Nablus Rafidia st</span>
+                        <span className="p-1 fw-bold phone">Phone: 092355555</span>  
+                        <span className="p-1 fw-bold email">Email: hireme@gmail.com </span>
                     </div> 
-                    <div class="col">
-                        <div><h4 class="social-title">Social media:</h4></div>
-                        <a href="#" class="fa fa-facebook"></a>
-                        <a href="#" class="fa fa-twitter"></a>
-                        <a href="#" class="fa fa-linkedin"></a>
+                    <div className="col">
+                        <div><h4 className="social-title">Social media:</h4></div>
+                        <a href="#" className="fa fa-facebook"></a>
+                        <a href="#" className="fa fa-twitter"></a>
+                        <a href="#" className="fa fa-linkedin"></a>
                     </div>
                 </div>
             </div>
         </section>
-        <div class="container">
-            <p class="lead copy-right">&copy; C.A.P. NNU </p>
+        <div className="container">
+            <p className="lead copy-right">&copy; C.A.P. NNU </p>
         </div>
     </footer>
     )
---
+++ job-app/src/components/modal/Modal.js
@@ -0,0 +1,9 @@
+
+const Modal =()=>{
+    return(
+    <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+    
+    </div>
+    )
+}
+export default Modal;
