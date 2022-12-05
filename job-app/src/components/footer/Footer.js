5f2c749987886dfe344cdb7c570c5d08bbf7148e
emad almassri <s12113576@stu.najah.edu>
2022-12-05 18:39:20 +0200

    reset floder name ,get back header and footer

--- job-app/src/components/footer/Footer.js
+++ job-app/src/components/footer/Footer.js
@@ -1,35 +1,33 @@
 import "./Footer.css";
 const Footer = () => {
     return (
-
-    <footer className="p-1 text-center">
-        <section id="contact-info" className="p-5">
-            <div className="container">
-                <div className="row g-4">
-                    <h2 className="text-center mb-3 contact-info-title">Contact Information </h2>
+    <footer class="p-1 text-center">
+        <section id="contact-info" class="p-5">
+            <div class="container">
+                <div class="row g-4">
+                    <h2 class="text-center mb-3 contact-info-title">Contact Information </h2>
                     <div className="logo-motto col">
                         <img className="footer-logo " src="./images/Logo-Footer.png" alt=""></img>
                         <h5><span className="footer-motto">Trust is all you need </span></h5>
                     </div>
                     < div className="footer-contact col">
-                        <span className="p-1 fw-bold loc">Location: Nablus Rafidia st</span>
-                        <span className="p-1 fw-bold phone">Phone: 092355555</span>  
-                        <span className="p-1 fw-bold email">Email: hireme@gmail.com </span>
+                        <span class="p-1 fw-bold loc">Location: Nablus Rafidia st</span>
+                        <span class="p-1 fw-bold phone">Phone: 092355555</span>  
+                        <span class="p-1 fw-bold email">Email: hireme@gmail.com </span>
                     </div> 
-                    <div className="col">
-                        <div><h4 className="social-title">Social media:</h4></div>
-                        <a href="#" className="fa fa-facebook"></a>
-                        <a href="#" className="fa fa-twitter"></a>
-                        <a href="#" className="fa fa-linkedin"></a>
+                    <div class="col">
+                        <div><h4 class="social-title">Social media:</h4></div>
+                        <a href="#" class="fa fa-facebook"></a>
+                        <a href="#" class="fa fa-twitter"></a>
+                        <a href="#" class="fa fa-linkedin"></a>
                     </div>
                 </div>
             </div>
         </section>
-        <div className="container">
-            <p className="lead copy-right">&copy; C.A.P. NNU </p>
+        <div class="container">
+            <p class="lead copy-right">&copy; C.A.P. NNU </p>
         </div>
     </footer>
-
     )
 }
 export default Footer;
--- job-app/src/pages/home/Home.js
+++ job-app/src/pages/home/Home.js
@@ -1,11 +1,11 @@
 
 import "./Home.css";
-import Footer from "../../components/Footer/Footer";
-import Header from "../../components/Header/Header";
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
 import NavBar from "../../components/NavBar/NavBar";
 
-import Services from "../../components/Services/Services";
-import AboutUs from "../../components/AboutUs/AboutUs";
+import Services from "../../components/services/Services";
+import AboutUs from "../../components/aboutUs/AboutUs";
 
 function Home() {
   return (
@@ -21,19 +21,19 @@ function Home() {
       <Services imag="./images/find.jpg" info="Find employees" paragraph="Companies struggle to locate employees,so we are in HireMe we
                 make it simple for you by showing employee resumes and providing
                 simple filtering so you can find the best possibilities as
-                quickly as possible." link="/Get-cv"/>
+                quickly as possible." link="/get-cv"/>
       </div>
       <div className='col-12 col-md-10 col-lg-6 col-xl-4 col-xxl-3'>
       <Services imag="./images/Filter.jpg" info="Filter" paragraph="   When reviewing resumes, it might be challenging to find the
                 information you're looking for, whether it's years of experience
                 or age. With a touch of a button, so we are in HireMe we've
                 given you fast ways to quickly find the ideal candidate. Here,
-                you'll find everything you need to hire the ideal candidate." link="/Get-cv"/>
+                you'll find everything you need to hire the ideal candidate." link="/get-cv"/>
       </div><div className='col'>
       <Services imag="./images/cv.jpg" info="Add CV" paragraph="Employees can submit a CV with ease by adding the necessary
                 information to the area on the page dedicated for it, and he can
                 change it at any moment by entering into his account, that he
-                will make it before attaching the CV." link="/Add-cv"/>
+                will make it before attaching the CV." link="/add-cv"/>
       </div>
       </div>
       </div>
