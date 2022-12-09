0255eee85f3509a565e189ebba7f60f0f81ba329
izzmoqbel <s12116328@stu.najah.edu>
2022-12-09 17:12:36 +0200

    make a component -select,date and url- and make the page responsive

--- job-app/src/components/Inputs/Phone.js
+++ job-app/src/components/Inputs/Phone.js
@@ -1,37 +1,37 @@
-import "./Inputs.css"
+import "./Inputs.css";
 
 import { useState } from "react";
 
 const Phone = (props) => {
+  const [number, setNumber] = useState("");
+  const [check, setCheck] = useState("");
 
-    const [number, setNumber] = useState("");
-    const [check, setCheck] = useState("");
+  const checkNumber = () => {
+    let stringNumber = String(number);
+    if (stringNumber.length !== 9) setCheck("error");
+    else setCheck("done");
+  };
 
-    const checkNumber = () => {
-        let stringNumber = String(number);
-        if (stringNumber.length !== 9) setCheck("error");
-        else setCheck("done");
-    };
-
-    return (
-        <>
-            <form>
-                <div className="form-number">
-                    <label>Phone Number</label>
-                    <input
-                        className="form-control form-control-lg"
-                        type="phone"
-                        placeholder="NUMBER"
-                        onChange={(e) => {
-                            setNumber(e.target.value);
-                            checkNumber();
-                        }}
-                    />
-                </div>
-                <p>{check}</p>
-            </form>
-        </>
-    );
+  return (
+    <>
+      <form>
+        <label className="label-form">{props.children} </label>
+        <div className="form">
+          <input
+            className="fab-TextInput "
+            type="phone"
+            placeholder={props.children}
+            onChange={(e) => {
+              setNumber(e.target.value);
+              checkNumber();
+            }}
+            required
+          />
+        </div>
+        <span className="validation">{check}</span>
+      </form>
+    </>
+  );
 };
 
 export default Phone;
---
+++ job-app/src/components/Inputs/URL.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Url = () => {
+  <>
+    <label className="label-form">Email</label>
+    <div className="form">
+      <input type="url" className="fab-TextInput " required />
+    </div>
+  </>;
+};
+export default Url;
