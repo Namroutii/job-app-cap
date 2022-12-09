06521a3e1e39708f912f7794572056d9f067503f
dalia khalili <khalilidalia58@gmail.com>
2022-12-09 22:48:32 +0200

    charts1

--- job-app/src/index.js
+++ job-app/src/index.js
@@ -5,21 +5,20 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home/Home";
-import AddCV from "./pages/addCV/AddCV";
-import Get from "./pages/getCV/GetCV";
-import Login from "./pages/login/Login";
+import Home from "./pages/Home/Home";
+import AddCV from "./pages/AddCV/AddCV";
+import Get from "./pages/GetCV/GetCV";
+import Login from "./pages/Login/Login";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
 
-        <Route path="/home" index element={<Home />} />
-        <Route path="/"  element={<Home />} />
-        <Route path="/add-cv" element={<AddCV />} />
-        <Route path="/get-cv" element={<Get />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/Home" index element={<Home />} />
+        <Route path="/Add-cv" element={<AddCV />} />
+        <Route path="/Get-cv" element={<Get />} />
+        <Route path="/Login" element={<Login />} />
 
       </Routes>
     </BrowserRouter>
