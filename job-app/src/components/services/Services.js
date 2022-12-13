4fc581b32bab303aa0b97559d59ac9f9570e7c6a
emad almassri <s12113576@stu.najah.edu>
2022-12-13 10:08:57 +0200

    change class name for service

--- job-app/src/components/services/Services.js
+++ job-app/src/components/services/Services.js
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 
 const Services = (props) => {
   return (
-    <section className="cards" id="services">
+    <section className="cardsServices" id="services">
       
       <div className="content">
         <Link to={props.link} className="services-links">
-          <div className="card">
-            <div className="imag">
+          <div className="cardServices">
+            <div className="imagServices">
               <img src={props.imag} alt="..."></img>
             </div>
 
-            <div className="info">
+            <div className="infoServices">
               <h2>{props.info}</h2>
               <p>
               {props.paragraph}
