bc871acbabec6f1611eaaab3e944770b468fb1ac
emad almassri <s12113576@stu.najah.edu>
2022-12-14 17:26:50 +0200

    rename className in service, fix navbars(delete div section ) ,style adminPanel card

--- job-app/src/components/services/Services.js
+++ job-app/src/components/services/Services.js
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 
 const Services = (props) => {
   return (
-    <section className="cardsServices" id="services">
+    <section className="cards-services" id="services">
       
       <div className="content">
         <Link to={props.link} className="services-links">
-          <div className="cardServices">
-            <div className="imagServices">
+          <div className="card-services">
+            <div className="imag-services">
               <img src={props.imag} alt="..."></img>
             </div>
 
-            <div className="infoServices">
+            <div className="info-services">
               <h2>{props.info}</h2>
               <p>
               {props.paragraph}
--- job-app/src/pages/AdminPanel/AdminPanel.js
+++ job-app/src/pages/AdminPanel/AdminPanel.js
@@ -1,17 +1,31 @@
+import AdminPanelCard from "../../components/AdminPanelCard/AdminPanelCard";
 import NavBar from "../../components/NavBar/NavBar";
+import "./AdminPanel.css";
+function AdminPanel() {
+    return (
 
-
-
-
-
-
-
-function AdminPanel(){
-return(
-
-    <div>
-        <NavBar/>
-    </div>
-)
+        <div>
+            
+            <div className="container">
+            <div className="row">
+                <NavBar />
+                </div>
+                <div className="row first-row">
+                    <div className="col-3">
+                        <AdminPanelCard />
+                    </div>
+                    <div className="col-3">
+                        <AdminPanelCard />
+                    </div>
+                    <div className="col-3">
+                        <AdminPanelCard />
+                    </div>
+                    <div className="col-3">
+                        <AdminPanelCard />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 export default AdminPanel;
