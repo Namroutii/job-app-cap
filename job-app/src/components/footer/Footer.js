c35419089ab970627dc8c4eedabb2d63ccfdca57
AbdullahSO <abd.s.othman@gmail.com>
2022-12-14 16:23:38 +0200

    fix footer

--- job-app/src/components/footer/Footer.js
+++ job-app/src/components/footer/Footer.js
@@ -1,66 +1,48 @@
 import "./Footer.css";
 const Footer = () => {
-  return (
-    <footer className="text-center">
-<<<<<<< HEAD
-        <section id="contact-info" className="p-1">
-            <div className="container">
-                <div className="row">
-                    <h2 className="text-center contact-info-title">Contact Information </h2>
-                    <div className="logo-motto col">
-                        <img className="footer-logo " src="./images/Logo-Footer.png" alt=""></img>
-                        <h5><span className="footer-motto">Trust is all you need </span></h5>
+    return (
+        <footer className="text-center">
+            <section id="contact-info">            
+                    <div className="row">
+                        <h2 className="text-center contact-info-title">Contact Information </h2>
+                        <div className="logo-motto col">
+                            <img className="footer-logo " src="./images/Logo-Footer.png" alt=""></img>
+                        </div>
+                        <address address className="footer-contact col">
+                            <span className="loc">Location: Nablus Rafidia st</span>
+                            <span className="phone">Phone: 092355555</span>  
+                            <span className="email">Email: hireme@gmail.com </span>
+                        </address> 
+                        <div className="col">
+                            <div><h4 className="social-title">Social media:</h4></div>
+                            <a href="https://ar-ar.facebook.com/" className="fa fa-facebook"></a>
+                            <a href="https://twitter.com/i/flow/login" className="fa fa-twitter"></a>
+                            <a href="https://www.linkedin.com/" className="fa fa-linkedin"></a>
+                        </div>
                     </div>
-                    < div className="footer-contact col">
-                        <span className="fw-bold loc">Location: Nablus Rafidia st</span>
-                        <span className="fw-bold phone">Phone: 092355555</span>  
-                        <span className="fw-bold email">Email: hireme@gmail.com </span>
-                    </div> 
-                    <div className="col">
-                        <div><h4 className="social-title">Social media:</h4></div>
-                        <a href="#" className="fa fa-facebook"></a>
-                        <a href="#" className="fa fa-twitter"></a>
-                        <a href="#" className="fa fa-linkedin"></a>
-                    </div>
-                </div>
-            </div>
-        </section>
-=======
-      <section id="contact-info">
->>>>>>> main
-        <div className="container">
-          <div className="row">
-            <h2 className="text-center contact-info-title">
-              Contact Information{" "}
-            </h2>
-            <div className="logo-motto col">
-              <img
-                className="footer-logo "
-                src="./images/Logo-Footer.png"
-                alt=""
-              ></img>
-              <h5>
-                <span className="footer-motto">Trust is all you need </span>
-              </h5>
-            </div>
-            <div className="footer-contact col">
-              <span className="fw-bold loc">Location: Nablus Rafidia st</span>
-              <span className="fw-bold phone">Phone: 092355555</span>
-              <span className="fw-bold email">Email: hireme@gmail.com </span>
-            </div>
-            <div className="col">
-              <div>
-                <h4 className="social-title">Social media:</h4>
-              </div>
-              <a href="#" className="fa fa-facebook"></a>
-              <a href="#" className="fa fa-twitter"></a>
-              <a href="#" className="fa fa-linkedin"></a>
-            </div>
-            <p className="lead copy-right">&copy; C.A.P. NNU </p>
-          </div>
-        </div>
-      </section>
-    </footer>
-  );
-};
+                <h4 className="lead copy-right text-center">&copy; C.A.P. NNU </h4>
+            </section>
+        </footer>
+        // <footer>
+        //     <h2 className="text-center contact-info-title">Contact Information</h2>
+        //     <div className="row">
+        //         <div className="logo col">
+        //             <img className="footer-logo " src="./images/Logo-Footer.png" alt=""></img>
+        //         </div>
+        //         <div className="footer-contact col">
+        //             <span className="loc">Location: Nablus Rafidia st</span>
+        //             <span className="phone">Phone: 092355555</span>  
+        //             <span className="email">Email: hireme@gmail.com </span>
+        //         </div>
+        //         <div className="col">
+        //             <h4 className="social-title">Social media:</h4>
+        //             <a href="https://ar-ar.facebook.com/" className="fa fa-facebook"></a>
+        //             <a href="https://twitter.com/i/flow/login" className="fa fa-twitter"></a>
+        //             <a href="https://www.linkedin.com/" className="fa fa-linkedin"></a>
+        //         </div>
+        //     </div>
+        //     <h4 className="lead copy-right text-center">&copy; C.A.P. NNU </h4>
+        // </footer>
+    )
+}
 export default Footer;
