2e5fb1d61517119a430413272c51ac4335ec4525
osamashouli <101651050+osamashouli@users.noreply.github.com>
2022-12-24 15:35:44 +0200

    making the table reusable and connecting it to the adminPanel

--- job-app/src/pages/getCV/GetCV.js
+++ job-app/src/pages/getCV/GetCV.js
@@ -5,6 +5,14 @@ import Footer from "../../components/footer/Footer";
 import NavBarGetCvs from "../../components/NavBarGetCvs/NavBarGetCvs";
 
 function getCV() {
+  const deletedIds = JSON.parse(localStorage.getItem("deletedCVs")) || [];
+  const filteredData = data.filter(
+    (obj1) => !deletedIds.some((obj2) => obj2[0].id === obj1.id)
+  );
+  // const reportedIds = JSON.parse(localStorage.getItem("reportedCVs")) || [];
+  // const filteredData = data.filter((obj1) =>
+  //   reportedIds.some((obj2) => obj2.id === obj1.id)
+  // );
   return (
     <div id="get-cv-container">
       <div id="nav-bar">
@@ -14,7 +22,13 @@ function getCV() {
         <img src="./images/get-cv-img.jpg" id="get-cv-img" />
         <h1 id="get-cv-header">Find Employees</h1>
       </div>
-      <Table data={data} rowsPerPage={23} />
+
+      <Table
+        data={filteredData}
+        rowsPerPage={23}
+        withFilter={true}
+        forPage="get-cv"
+      />
 
       <Footer />
     </div>
